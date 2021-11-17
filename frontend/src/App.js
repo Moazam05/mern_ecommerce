53cb6dba8fef0a9222b05cd48dce65c9e4080271
Moazam05 <salmanmoazam08@gmail.com>
2021-11-18 01:00:37 +0500

    react 17 & router-dom 6 version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <HomeScreen />{' '}
-        </Container>
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -7,14 +8,16 @@ function Header() {
       <header>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
           <Container>
-            <Navbar.Brand href='/'>Mern Ecommerce</Navbar.Brand>
+            <Link to='/'>
+              <Navbar.Brand>Mern Ecommerce</Navbar.Brand>
+            </Link>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
               <Nav className='ml-auto'>
-                <Nav.Link href='/cart'>
+                <Nav.Link as={Link} to='/cart'>
                   <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
-                <Nav.Link href='/login'>
+                <Nav.Link as={Link} to='/login'>
                   <i className='fas fa-user'></i> Sign In
                 </Nav.Link>
               </Nav>
